055e5159e95d041d7cdc42560b5b53ec5e51e29f
Russ MacShane <rmacshane@liquidweb.com>
2026-02-27 15:10:23 -0600

    Added Lsps

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason
 
 ---@type LazySpec
@@ -13,9 +11,13 @@ return {
       ensure_installed = {
         -- install language servers
         "lua-language-server",
+        "eslint-lsp",
+        "typescript-language-server",
 
-        -- install formatters
+        -- install formatters/linters
         "stylua",
+        "prettier",
+        "eslint_d",
 
         -- install debuggers
         "debugpy",
